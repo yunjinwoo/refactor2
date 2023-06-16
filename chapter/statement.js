f401e7ff8674d8781e1254fc29ea733cf07ee452
yunjinwoo <jwyoon@bizwizsystem.co.kr>
2023-06-16 15:34:53 +0900

    [chapter] - amountFor 추가

--- chapter/statement.js
+++ chapter/statement.js
@@ -1,3 +1,26 @@
+function amountFor(perf, play){
+  let thisAmount = 0;
+  switch (play.type) {
+    case "tragedy": //비극
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case "comedy": //희극
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 1000 + 500 * (perf.audience - 30);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`알수없는 장르:${play.type}`);
+  }
+  return thisAmount
+}
+
+
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -10,25 +33,7 @@ function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
-
-    switch (play.type) {
-      case "tragedy": //비극
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy": //희극
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 1000 + 500 * (perf.audience - 30);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`알수없는 장르:${play.type}`);
-    }
+    let thisAmount = amountFor(perf, play);
 
     //포인트를 적립한다
     volumeCredits += Math.max(perf.audience - 30, 0);
