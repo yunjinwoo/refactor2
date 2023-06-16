855e9cd475215918cfabd0802f0db0becb125823
yunjinwoo <jwyoon@bizwizsystem.co.kr>
2023-06-16 16:40:46 +0900

    totalVolumeCredits 총액 함수  추가

--- chapter/statement.js
+++ chapter/statement.js
@@ -1,26 +1,34 @@
 function statement(invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `청구내역(고객명:${invoice.customer})\n`;
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
-
+  
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf)
-
     //청구 내역을 출력한다.
-    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `총액: ${format(totalAmount / 100)}\n`;
+  
+  let volumeCredits = totalVolumeCredits()
+  result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
 
+  function totalVolumeCredits(){
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(perf)
+    }
+    return volumeCredits;
+  }
+  function usd(aNumber){
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    }).format(aNumber/100)
+  }
   function volumeCreditsFor(perf) {
     let result = 0;
     //포인트를 적립한다
