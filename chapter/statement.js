3648924a62de6cb167368ba510ea8be80ca0e76e
yunjinwoo <jwyoon@bizwizsystem.co.kr>
2023-06-16 16:45:48 +0900

    totalAmount 추가로 합계 - 총액 인라인

--- chapter/statement.js
+++ chapter/statement.js
@@ -1,33 +1,42 @@
+/**
+ * 1. 반복문 쪼개기 - 변수값을 누적시키는 부분을 분리한다
+ * 2. 문장 슬라이드하기 - 변수 초기화 문장을 변수 값 누적코드 바로 앞으로 옮긴다
+ * 3. 함수 추출하기 - 적립포인트 계산 부분을 별도 함수로 추출한다
+ * 4. 변수 인라인하기 - volumCredits 변수를 제거한다
+ * **/
 function statement(invoice, plays) {
-  let totalAmount = 0;
   let result = `청구내역(고객명:${invoice.customer})\n`;
-  
   for (let perf of invoice.performances) {
     //청구 내역을 출력한다.
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
-    totalAmount += amountFor(perf);
   }
-  
-  let volumeCredits = totalVolumeCredits()
-  result += `총액: ${usd(totalAmount)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
+
+  result += `총액: ${usd(totalAmount())}\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 
-  function totalVolumeCredits(){
+  function totalAmount(){
+    let totalAmount = 0; 
+    for (let perf of invoice.performances) {
+      totalAmount += amountFor(perf);
+    }
+    return totalAmount
+  }
+  function totalVolumeCredits() {
     let volumeCredits = 0;
     for (let perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf)
+      volumeCredits += volumeCreditsFor(perf);
     }
     return volumeCredits;
   }
-  function usd(aNumber){
+  function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
       minimumFractionDigits: 2,
-    }).format(aNumber/100)
+    }).format(aNumber / 100);
   }
   function volumeCreditsFor(perf) {
     let result = 0;
