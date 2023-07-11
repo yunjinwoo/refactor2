d940b3a2443ee0688c4a23688f325c774eeeaaea
yunjinwoo <jwyoon@bizwizsystem.co.kr>
2023-07-11 13:05:04 +0900

    P119 전체코드

--- functional/index.js
+++ functional/index.js
@@ -1,23 +1,90 @@
 
-var shopping_cart = [];
-var shopping_cart_total = 0;
+let shopping_cart = [];
+let shopping_cart_total = 0;
 
+add_item_to_cart('test1',5)
+add_item_to_cart('test2',5)
+add_item_to_cart('test3',10)
+add_item_to_cart('test4',2)
+console.log('shopping_cart', shopping_cart)
+
+console.log('shopping_cart_total', shopping_cart_total)
+//P119 전체 코드를 봅시다
 function add_item_to_cart(name, price){
-    shopping_cart.push({
-        name:name,
-        price:price
-    })
+    shopping_cart = add_item(shopping_cart, name, price)
     calc_cart_total();
 }
 
 function calc_cart_total(){
-    shopping_cart_total = 0;
-    for(var i = 0 ; i < shopping_cart.length ; i++ ){
-        var item = shopping_cart[i];
-        shopping_cart_total += item.price;
-    }
+    shopping_cart_total = calc_total(shopping_cart);
     set_cart_total_dom();
+    update_shipping_icons();
+    update_tax_dom();
+}
+
+function update_shipping_icons(){
+    let buy_buttons = get_buy_buttons_dom();
+    buy_buttons?.forEach((button)=>{
+        let item = button.item;
+
+        if(gets_free_shipping(shopping_cart_total, item.price)){
+            button.show_free_shipping_icon();
+        }else{
+            button.hide_free_shipping_icon();
+        }
+    })
+}
+
+function update_tax_dom(){
+    set_tax_dom(calc_tax(shopping_cart_total))
+}
+
+//===============================
+
+function add_item(cart, name, price){
+    let new_cart = cart.slice();
+    new_cart.push({
+        name, price
+    })
+    return new_cart;
+}
+function calc_total(cart){    
+    let total = 0;
+    cart?.forEach((item)=>{
+        total += item.price;
+    })
+    return total
 }
 
 
-function set_cart_total_dom(){}
+function gets_free_shipping(total, item_price){
+    return item_price + total >= 20
+}
+
+function calc_tax(amount){
+    return amount*0.10
+}
+
+//===================== 껍데기 함수
+function set_cart_total_dom(){
+    console.log('set_cart_total_dom - emtpy')
+}
+function set_tax_dom(){
+    console.log('set_tax_dom - emtpy')
+}
+function get_buy_buttons_dom(){
+    //console.log('get_buy_buttons_dom - emtpy')
+    let obj = {
+        item:shopping_cart[0],
+        show_free_shipping_icon:()=>{ console.log('icon - show') },
+        hide_free_shipping_icon:()=>{ console.log('icon - hide') }
+    }
+    return [obj]
+}
+//===================== 껍데기 함수 END
+
+
+
+
+
+//=============================
